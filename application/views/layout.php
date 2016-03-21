2bf29d2353ff8387aaec23744d28c6711ada5b3f
ibadboy <polawat.th@gmail.com>
2016-03-21 23:07:44 +0700

    add link source code

--- application/views/layout.php
+++ application/views/layout.php
@@ -28,7 +28,7 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
        <li>
-           &nbsp;
+           <?php echo anchor('https://github.com/MrDynamic/random','Source Code'); ?>
        </li>
       </ul>
     </div><!-- /.navbar-collapse -->
